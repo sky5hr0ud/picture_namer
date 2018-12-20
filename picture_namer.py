9802c7c0347e6cf9dfeb78e18c12ee57202d2675
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2018-12-19 19:07:31 -0800

    Update picture_namer.py

--- picture_namer.py
+++ picture_namer.py
@@ -2,34 +2,35 @@
 # -*- coding: utf-8 -*-
 #
 #  picture_namer.py
-#  
+#
 #  Copyright 2018 sky5hr0ud <sky5hr0ud>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+#
 #  This script appends the folder name to picture filenames.
 
 import os
-import sys
+
 
 def main():
 	path = input('Folder path: ')
 	os.chdir(path)
 	file_namer(path)
 	return 0
+
 
 def file_namer(path):
 	count = file_counter()
@@ -45,8 +46,9 @@ def file_namer(path):
 				new_file = new_file + str(count).zfill(5) + '_'
 				os.rename(file, new_file+file)
 				count = count + 1
-			
-def file_counter(): #returns the number of files in a directory
+
+
+def file_counter():  # returns the number of files in a directory
 	count = 0
 	for file in os.listdir('.'):
 		#if file.startswith(os.path.relpath(".","..")):
@@ -54,5 +56,6 @@ def file_counter(): #returns the number of files in a directory
 		if file.startswith(rel_path):
 			count = count + 1
 	return count
+
 
 main()
