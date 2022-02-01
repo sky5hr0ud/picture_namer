d322619f7a1dc439936cb3a218800ef51178b54c
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2022-02-01 13:52:49 -0800

    build flag error
    GUI hung when renaming hundreds of files
    
    removed --windowed in the build command and it worked

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 :: This uses pyinstaller to build a executable. Requires Python and pyinstaller.
 :: After the executable is created the build directory and .spec files are cleaned up.
 REM Building picture_namer.exe using pyinstaller
-pyinstaller --windowed .\picture_namer_gui.spec
+pyinstaller .\picture_namer_gui.spec
 REM Deleting files and directories.
 rmdir build /s /q
